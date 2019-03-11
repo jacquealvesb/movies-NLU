093708508628740f0281e2cb26723c3a1ef615af
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-03-11 11:21:41 -0300

    Getting movie ID

--- moviesNLU/Controller/ViewController.swift
+++ moviesNLU/Controller/ViewController.swift
@@ -11,15 +11,10 @@ import UIKit
 let apiKey = "094fd8f84048425f068f6965ca8bb6af"
 
 class ViewController: UIViewController {
-
-    let movie = "Jack Reacher"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        getMovieID(of: movie) { (id) in
-            print("\n\n\nMovide ID: \(id)")
-        }
     }
 
 
