628d76f487a3984457acfb59c3c831d3f87593d2
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-03-13 09:32:17 -0300

    Movie model

---
+++ moviesNLU/Model/Movie.swift
@@ -0,0 +1,52 @@
+//
+//  Movie.swift
+//  moviesNLU
+//
+//  Created by Jacqueline Alves on 13/03/19.
+//  Copyright © 2019 jacqueline.alves.moviesNLU. All rights reserved.
+//
+
+import Foundation
+
+class Movie {
+    var posterPath: String
+    var adult: Bool
+    var overview: String
+    var releaseDate: Date?
+    var genreIDs: [Int]
+    var id: Int
+    var language: String
+    var originalTitle: String
+    var title: String
+    var backdropPath: String
+    var popularity: Double
+    var voteCount: Int
+    var video: Bool
+    var voteAvarage: Double
+    
+    init(withDictionary dict: [String:Any]) {
+        self.posterPath = dict["poster_path"] as? String ?? ""
+        self.adult = dict["adult"] as? Bool ?? false
+        self.overview = dict["overview"] as? String ?? ""
+        
+        if let releaseDateString = dict["release_date"] as? String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "yyyy-MM-dd"
+            
+            self.releaseDate = dateFormatter.date(from: releaseDateString)
+        } else {
+            self.releaseDate = nil
+        }
+        
+        self.genreIDs = dict["genre_ids"] as? [Int] ?? []
+        self.id = dict["id"] as? Int ?? -1
+        self.language = dict["language"] as? String ?? "en"
+        self.originalTitle = dict["original_title"] as? String ?? ""
+        self.title = dict["title"] as? String ?? ""
+        self.backdropPath = dict["backdrop_path"] as? String ?? ""
+        self.popularity = dict["popularity"] as? Double ?? 0.0
+        self.voteCount = dict["vote_count"] as? Int ?? 0
+        self.video = dict["video"] as? Bool ?? false
+        self.voteAvarage = dict["vote_average"] as? Double ?? 0.0
+    }
+}
