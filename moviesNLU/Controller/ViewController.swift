4e5aa8f6cc7031a536f12e7740aefed7be72260d
Jacqueline Alves <jacque.alves.b@gmail.com>
2019-03-14 08:51:31 -0300

    App interface

--- moviesNLU/Controller/ViewController.swift
+++ moviesNLU/Controller/ViewController.swift
@@ -10,11 +10,47 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var movieTextField: UITextField!
+    @IBOutlet weak var analysisCard: AnalysisCardView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.analysisCard.transform = CGAffineTransform(translationX: 0, y: self.analysisCard.frame.size.height)
+        self.analysisCard.closeButton.addTarget(self, action: #selector(hideCard), for: .touchUpInside)
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissViewOrKeyboard))
+        view.addGestureRecognizer(tap)
+        
+        self.movieTextField.delegate = self
     }
 
+    func showCard() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.analysisCard.transform = CGAffineTransform(translationX: 0, y: 0)
+        }, completion: nil)
+    }
+    
+    @objc func hideCard() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.analysisCard.transform = CGAffineTransform(translationX: 0, y: self.analysisCard.frame.size.height)
+        }, completion: nil)
+    }
+    
+    @objc func dismissViewOrKeyboard() {
+        if(movieTextField.isFirstResponder) {
+            view.endEditing(true)
+        } else {
+            hideCard()
+        }
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func analyzeMovie(_ sender: Any) {
+        dismissViewOrKeyboard()
+        showCard()
+    }
 
 }
 
